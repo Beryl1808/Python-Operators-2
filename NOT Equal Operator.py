e7396b5b0cf341a543a1d23ff471191b74f4300c
Beryl1808 <dkwafo17@gmail.com>
2024-12-15 19:53:55 +0000

    2commit

---
+++ NOT Equal Operator.py
@@ -0,0 +1,24 @@
+a = 23
+b = 34
+c = 22
+
+print(a != b)
+print(b !=c)
+
+
+a="python"
+b="coding"
+
+if a != b :
+    print(a, "and", b, "are different.")
+
+a=10
+b= 12
+
+if (a==10) != (b==12) :
+    print("Helo")
+
+a= int(input("enter a number"))
+
+if a%2 != 0:
+    print(a,"is not even")
